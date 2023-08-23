4d3e3682ceb06f10e55f9d066498e25a80809526
Flopiras <Flopiras@yahoo.it>
2023-08-23 16:24:03 +0200

    stamp trains table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,35 @@
 @section('title', 'Home')
 
 @section('main')
+<div class="container mt-4">
 
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Train</th>
+            <th scope="col">From</th>
+            <th scope="col">Departure Time</th>
+            <th scope="col">To</th>
+            <th scope="col">Arrival Time</th>
+            <th scope="col">In Time</th>
+            <th scope="col">Deleted</th>
+          </tr>
+        </thead>
+        <tbody>
+        @foreach($trains as $train)
+            
+          <tr>
+            <th scope="row">{{$train['company']}}</th>
+            <td>{{$train['departure']}}</td>
+            <td>{{$train['departure_time']}}</td>
+            <td>{{$train['arrival']}}</td>
+            <td>{{$train['arrival_time']}}</td>
+            <td>{{$train['is_in_time']}}</td>
+            <td>{{$train['is_deleted']}}</td>
+          </tr>
+        @endforeach
+          
+        </tbody>
+      </table>
+</div>
 @endsection
